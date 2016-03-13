b9840d6fdbb0c171b3b41249673f00c090396512
Yash Srivastav <yashsriv01@gmail.com>
2016-03-13 12:04:54 +0530

    Made board representation

---
+++ src/board.py
@@ -0,0 +1,304 @@
+EMPTY = 'empty'
+BLACK = 'black'
+WHITE = 'white'
+class board():
+
+    def __init__(self):
+        self.b = [[EMPTY for x in range(8)] for y in range(8)]  # Create 2d board
+        self.b[3][3] = WHITE
+        self.b[4][4] = WHITE
+        self.b[3][4] = BLACK
+        self.b[4][3] = BLACK
+        self.turn = BLACK
+        self.legal_moves = []
+        self.calc_legal_moves()
+
+    def display_board(self):
+        for i in range(8):
+            for j in range(8):
+                if(self.b[i][j] == EMPTY):
+                    print('.',end = ' ')
+                elif(self.b[i][j] == BLACK):
+                    print('X',end = ' ')
+                elif(self.b[i][j] == WHITE):
+                    print('O',end = ' ')
+            print('')
+
+    def is_legal_move(self,mov):
+        (x, y) = mov
+        if mov in self.legal_moves:
+            return True
+        else:
+            return False
+
+    def change_turn(self):
+        if self.turn == BLACK:
+            self.turn = WHITE
+        else :
+            self.turn = BLACK
+
+    def is_game_over(self):
+        if len(self.legal_moves) == 0:
+            return True
+        for i in range(8):
+            for j in range(8):
+                if self.b[i][j] == EMPTY:
+                    return False
+        return True
+
+    def calc_legal_moves(self):
+        self.legal_moves = []
+        for i in range(8):
+            for j in range(8):
+                if self.b[i][j] == self.turn:
+                    self.check_pos(i,j)
+
+    def check_pos(self, row, col):
+
+        # Horizontal right check
+        x = row
+        y = col + 1
+        count = 0
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY and count != 0:
+                self.legal_moves.append((x, y))
+                break
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                break
+            else :
+                count += 1
+            y += 1
+        # Horizontal left check
+        x = row
+        y = col - 1
+        count = 0
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY and count != 0:
+                self.legal_moves.append((x, y))
+                break
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                break
+            else :
+                count += 1
+            y -= 1
+        # Vertical down check
+        x = row + 1
+        y = col
+        count = 0
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY and count != 0:
+                self.legal_moves.append((x, y))
+                break
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                break
+            else :
+                count += 1
+            x += 1
+        # Vertical up check
+        x = row - 1
+        y = col
+        count = 0
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY and count != 0:
+                self.legal_moves.append((x, y))
+                break
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                break
+            else :
+                count += 1
+            x -= 1
+        # Diagonal NW check
+        x = row - 1
+        y = col - 1
+        count = 0
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY and count != 0:
+                self.legal_moves.append((x, y))
+                break
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                break
+            else :
+                count += 1
+            x -= 1
+            y -= 1
+        # Diagonal NE check
+        x = row - 1
+        y = col + 1
+        count = 0
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY and count != 0:
+                self.legal_moves.append((x, y))
+                break
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                break
+            else :
+                count += 1
+            x -= 1
+            y += 1
+
+        # Diagonal SW check
+        x = row + 1
+        y = col - 1
+        count = 0
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY and count != 0:
+                self.legal_moves.append((x, y))
+                break
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                break
+            else :
+                count += 1
+            x += 1
+            y -= 1
+
+        # Diagonal SE check
+        x = row + 1
+        y = col + 1
+        count = 0
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY and count != 0:
+                self.legal_moves.append((x, y))
+                break
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                break
+            else :
+                count += 1
+            x += 1
+            y += 1
+
+    def shift_color(self, l):
+        for pos in l:
+            self.b[pos[0]][pos[1]] = self.turn
+
+    def make_move(self, row, col):
+        self.b[row][col] = self.turn
+
+        # Horizontal right check
+        x = row
+        y = col + 1
+        count = []
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                self.shift_color(count)
+                break
+            else :
+                count.append((x, y))
+            y += 1
+        # Horizontal left check
+        x = row
+        y = col - 1
+        count = []
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                self.shift_color(count)
+                break
+            else :
+                count.append((x, y))
+            y -= 1
+        # Vertical down check
+        x = row + 1
+        y = col
+        count = []
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                self.shift_color(count)
+                break
+            else :
+                count.append((x, y))
+            x += 1
+        # Vertical up check
+        x = row - 1
+        y = col
+        count = []
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                self.shift_color(count)
+                break
+            else :
+                count.append((x, y))
+            x -= 1
+        # Diagonal NW check
+        x = row - 1
+        y = col - 1
+        count = []
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                self.shift_color(count)
+                break
+            else :
+                count.append((x, y))
+            x -= 1
+            y -= 1
+        # Diagonal NE check
+        x = row - 1
+        y = col + 1
+        count = []
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                self.shift_color(count)
+                break
+            else :
+                count.append((x, y))
+            x -= 1
+            y += 1
+
+        # Diagonal SW check
+        x = row + 1
+        y = col - 1
+        count = []
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                self.shift_color(count)
+                break
+            else :
+                count.append((x, y))
+            x += 1
+            y -= 1
+
+        # Diagonal SE check
+        x = row + 1
+        y = col + 1
+        count = []
+        while y < 8 and y > -1 and x > -1 and x < 8:
+            if self.b[x][y] == EMPTY:
+                break;
+            if self.b[x][y] == self.turn:
+                self.shift_color(count)
+                break
+            else :
+                count.append((x, y))
+            x += 1
+            y += 1
+
+
+
